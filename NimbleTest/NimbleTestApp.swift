c1791df3fdd0246a10af9d2b7fafd6dd607dbd27
David Uebelacker <david@uebelacker.ch>
2023-01-13 16:16:27 +0100

    Initial Commit

---
+++ NimbleTest/NimbleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbleTestApp.swift
+//  NimbleTest
+//
+//  Created by David Uebelacker on 13.01.23.
+//
+
+import SwiftUI
+
+@main
+struct NimbleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
